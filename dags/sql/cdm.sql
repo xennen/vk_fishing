589c79aba6b094fb69dc71cd0c5768b1acc00b88
xennen <jesterun22@gmail.com>
2024-05-17 00:16:05 +0300

    cdm sql

---
+++ dags/sql/cdm.sql
@@ -0,0 +1,20 @@
+---Топ - 5 самых популярных имен
+SELECT first_name, COUNT(first_name) as count
+  FROM dds.user
+ GROUP BY first_name
+ ORDER BY COUNT(first_name) DESC
+ LIMIT 5;
+
+---Выдать топ-3 города, в которых среднее кол-во друзей участников группы самое наибольшее
+SELECT town, AVG(friends_count) AS avg_friends_count
+  FROM dds.user
+ GROUP BY town
+ ORDER BY avg_friends_count DESC;
+
+---Какой город самый часто встречаемый у участников этой группы
+SELECT town, count(first_name) as members_from_town
+  FROM dds.user
+ WHERE town <> ''
+ GROUP BY 1
+ ORDER BY 2 DESC
+ LIMIT 1;
